006b3510cdbe3bd04c36d48054458d5ca79a52b8
telmo <telmo@email.com>
2026-03-04 11:15:12 -0300

    Escrita função de processos pai e filho

---
+++ novo_processo.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+int novo_processo(char **args){
+    pid_t pid;
+    int status;
+
+    pid = fork();
+
+    if (pid == 0){
+        //processo filho
+        if(execvp(args[0], args) == -1){
+            perror("Erro no novo processo: processo_filho");
+        }
+        exit(EXIT_FAILURE);
+    }
+    else if(pid < 0){
+        perror("Erro no novo processo: fork()");
+    }
+    else {
+        //processo pai
+        do {
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return (-1);
+}
